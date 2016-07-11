8ffd9e97812a32baa58ea70bb792bd6c0b461a1e
Taha <muhammadtahakhan222@gmail.com>
2016-07-11 23:25:51 +0500

    test of input and output eventemiter in working

--- app/components/main/app.component.ts
+++ app/components/main/app.component.ts
@@ -4,16 +4,19 @@ import {TodosService} from './../../services/todos.service';
 
 @Component({
   selector: 'my-app',
-  template: `<h1> Todolist</h1>
-  <input type="text" #parent (keyup)="0"/>{{parent.value}}
+  template: `<h1> Todolist</h1>{{childdata}}
+  <input type="text" #parent (keyup)="0"/>
   <button (click)="send()">send</button>
-  <todos [test]="parent"></todos>`,
+  <todos (myevent)="childdata=$event" [test]="parent"></todos>
+  
+  `,
   directives: [TodosComponent],
   providers: [TodosService]
 })
 export class AppComponent {
 
- 
+ public childdata:any;
+
    constructor(){
      
    }
--- app/components/todos/todos.component.ts
+++ app/components/todos/todos.component.ts
@@ -1,27 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import {TodosService} from './../../services/todos.service';
 
 
 @Component({
   selector: 'todos',
   template: `<h1>todolist</h1>
-            inter todo:  <input type="text" #item />
+            inter todo:  <input type="text" #item (keyup)="onChange(item.value)" />
             <button (click)="addtodo(item)">Add</button>
               <ul>
               <li *ngFor='let items of todos let i=index' >{{items}}  <button (click)="deletetodo(i)">Delete</button></li>
               </ul>{{test.value}}
   `,
- inputs: ['test']
+ inputs: ['test'],
+ outputs:['myevent']
 
 })
 export class TodosComponent {
  public test:any;
  public todos:any;
+  myevent = new EventEmitter();
 
 constructor(private todosService:TodosService){
   this.todos = todosService.gettodos();
  
 }
+
+onChange(child:string){
+this.myevent.emit(child);
+console.log(child);
+}
+
  addtodo(todoitem){
    console.log(todoitem.value);
    this.todos.push(todoitem.value);
